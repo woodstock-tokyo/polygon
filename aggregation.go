55659af9aa211cca35ccdc10f0bbf5150efc8f26
ilovelili <route666@live.cn>
2022-02-06 21:57:06 +0900

    add aggregation timestamp

--- aggregation.go
+++ aggregation.go
@@ -8,27 +8,36 @@ import (
 
 // Aggregation Get aggregate bars for a ticker over a given date range in custom time window sizes
 type Aggregation struct {
-	Ticker       string `json:"ticker"`
-	QueryCount   int    `json:"queryCount"`
-	ResultsCount int    `json:"resultsCount"`
-	Adjusted     bool   `json:"adjusted"`
-	Results      []struct {
-		Open                   float64 `json:"o"`
-		Close                  float64 `json:"c"`
-		High                   float64 `json:"h"`
-		Low                    float64 `json:"l"`
-		TransactionNumber      int     `json:"n"`
-		Volume                 float64 `json:"v"`
-		VolumeWeightedAvgPrice float64 `json:"vw"`
-	} `json:"results"`
-	Status    string `json:"status"`
-	RequestID string `json:"request_id"`
-	Count     int    `json:"count"`
+	Ticker       string              `json:"ticker"`
+	QueryCount   int                 `json:"queryCount"`
+	ResultsCount int                 `json:"resultsCount"`
+	Adjusted     bool                `json:"adjusted"`
+	Results      []AggregationResult `json:"results"`
+	Status       string              `json:"status"`
+	RequestID    string              `json:"request_id"`
+	Count        int                 `json:"count"`
+}
+
+// AggregationResult aggregation result item
+type AggregationResult struct {
+	Open                   float64 `json:"o"`
+	Close                  float64 `json:"c"`
+	High                   float64 `json:"h"`
+	Low                    float64 `json:"l"`
+	TransactionNumber      int     `json:"n"`
+	Volume                 float64 `json:"v"`
+	VolumeWeightedAvgPrice float64 `json:"vw"`
+	Timestamp              int64   `json:"t"`
 }
 
 // Valid check whether aggregation is valid or not
 func (a Aggregation) Valid() bool {
 	return (a.Status == "OK" || a.Status == "DELAYED") && a.Count > 0
+}
+
+// Valid check whether aggregation is valid or not
+func (ar AggregationResult) Time() time.Time {
+	return time.UnixMilli(ar.Timestamp)
 }
 
 // AggregationOption aggregation option
