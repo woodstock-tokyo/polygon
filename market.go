2642dc0861ba48c4f248e00876b89f20c0943455
ilovelili <route666@live.cn>
2022-06-13 17:58:41 +0900

    add market tostring method

--- market.go
+++ market.go
@@ -20,6 +20,43 @@ type Market struct {
 	} `json:"currencies"`
 }
 
+func (m Market) String(market ...string) string {
+	_market := "stock"
+	if len(market) == 1 {
+		_market = market[0]
+	}
+
+	switch _market {
+	case "stock":
+		if m.Status == Open {
+			return string(Open)
+		}
+
+		if m.Status == Closed {
+			return string(Closed)
+		}
+
+		if m.EarlyHours {
+			return string(EarlyHours)
+		}
+
+		if m.AfterHours {
+			return string(AfterHours)
+		}
+
+		return ""
+
+	case "crypto":
+		return string(m.Currencies.Crypto)
+
+	case "forex":
+		return string(m.Currencies.FX)
+
+	default:
+		return ""
+	}
+}
+
 type MarketOption struct{}
 
 // MarketStatus Get the current trading status of the exchanges and overall financial markets.
